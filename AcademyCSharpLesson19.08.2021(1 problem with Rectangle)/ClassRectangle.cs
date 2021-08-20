f407ff29fc43531609e2397129db3a0686e5a8b2
Amir Ibragimov <ibragimov-amir@list.ru>
2021-08-20 16:40:14 +0500

    19.08.2021 with 2 projects (Rectangle and Book)

---
+++ AcademyCSharpLesson19.08.2021(1 problem with Rectangle)/ClassRectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace AcademyCSharpLesson19._08._2021_1_problem_with_Rectangle_
+{
+    class ClassRectangle
+    {
+        static void Main()
+        {
+            Console.Write("Введите число длины прямоугольника: ");
+            var side1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write('\n');
+
+            Console.Write("Введите число ширины прямоугольника: ");
+            var side2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write('\n');
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Вычисления площади прямоугольника: { rectangle.AreaCalculator()}");
+            Console.Write('\n');
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.PerimeterCalculator()}");
+
+        }
+    }
+    class Rectangle
+    {
+        public double side1;
+        public double side2;
+
+        public double Area
+        {
+            get
+            {
+                return side1 * side2;
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return 2 * (side1 + side2);
+            }
+        }
+
+        public double AreaCalculator()
+        {
+            return Area;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return Perimeter;
+        }
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+    }
+}
---
+++ AcademyCSharpLesson19.08.2021(2 problem with Book)/Book.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace AcademyCSharpLesson19._08._2021_2_problem_with_Book_
+{
+    class Book
+    {
+        static void Main()
+        {
+            Console.Write("Введите Название книги: ");
+            var titleText = Console.ReadLine();
+            Console.Write('\n');
+
+            Console.Write("Введите Имя Автора книги: ");
+            var authorText = Console.ReadLine();
+            Console.Write('\n');
+
+            Console.Write("Введите контент книги: ");
+            var contentText = Console.ReadLine();
+            Console.Write('\n');
+
+            var title = new Title(titleText);
+            var author = new Author(authorText);
+            var content = new Content(contentText);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Название книги: {title.Show()}");
+            Console.ResetColor();
+            Console.Write('\n');
+
+
+            Console.WriteLine($"Имя Автора книги: {author.Show()}");
+            Console.ResetColor();
+            Console.Write('\n');
+
+            Console.WriteLine($"Контент книги: {content.Show()}");
+            Console.ResetColor();
+        }
+    }
+    class Title
+    {
+        public string title;
+
+        public Title(string title)
+        {
+            this.title = title;
+        }
+
+        public string Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return title;
+        }
+    }
+    class Author
+    {
+        public string author;
+        public Author(string author)
+        {
+            this.author = author;
+        }
+        public string Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return author;
+        }
+    }
+    class Content
+    {
+        public string content;
+        public Content(string content)
+        {
+            this.content = content;
+        }
+        public string Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            return content;
+        }
+    }
+}
