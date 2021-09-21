fc0d6d16aab0c816fe26f33a9a179553a3b63bee
Amir Ibragimov <ibragimov-amir@list.ru>
2021-09-21 16:32:33 +0500

    16.09.2021 Added "TransactionAccount.sql"

---
+++ AcademyCSharpLesson18.09.2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcademyCSharpLesson18._09._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
