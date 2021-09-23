c5cb40e918e567a1f2cb2440bfeab596f9fee1f2
Amir Ibragimov <ibragimov-amir@list.ru>
2021-09-24 00:22:59 +0500

    21.09.2021 List, Dictionary

--- AcademyCSharpLesson21.09.2021/Program.cs
+++ AcademyCSharpLesson21.09.2021/Program.cs
@@ -1,46 +1,150 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AcademyCSharpLesson21._09._2021
 {
-    class AccountTansactions
+
+    class HomeLesson
     {
         static void Main()
         {
-            var conString = @"Data Source = .\Dev; initial Catalog = TransactionAccount; Integrated Security = true";
+            Console.WriteLine("Первая программа LIST");
+            // 1 Задача
+            List<SmartPhones<string, decimal>> smartPhones = new List<SmartPhones<string, decimal>>();
 
-            var working = true;
+            smartPhones.Add(new SmartPhones<string, decimal> { Kachestvo = "Nice", Model = "Samsung", Price = 1000m });
+            smartPhones.Add(new SmartPhones<string, decimal> { Kachestvo = "Nice", Model = "Iphone", Price = 1000m });
+            smartPhones.Add(new SmartPhones<string, decimal> { Kachestvo = "Normal", Model = "Xiaomi", Price = 950m });
+            smartPhones.Add(new SmartPhones<string, decimal> { Kachestvo = "Normal", Model = "Huawei", Price = 700m });
+
+            bool working = true;
             while (working)
             {
-                Console.WriteLine("1. Exit\n2. Create Account\n3. Show Accounts\n4. Transfer From One Account To Another Account\nChoice:");
-                Int32.TryParse(Console.ReadLine(), out var choice);
+                Console.WriteLine("1.Выйти\n2.Добавить Телефон\n3.Выберать Телефон по индексу\n4.Посчитать общее количество Телефонов\nВыбор:");
+                int.TryParse(Console.ReadLine(), out var choice);
                 switch (choice)
                 {
                     case 1:
                         working = false;
                         break;
                     case 2:
-                        CreateAccount(conString);
+                        {
+                            Console.Write("Качество: ");
+                            var kachestvo = Console.ReadLine();
+                            Console.Write("Модель телефона: ");
+                            var model = Console.ReadLine();
+                            Console.Write("Цена: ");
+                            Decimal.TryParse(Console.ReadLine(), out var price);
+                            smartPhones.Add(new SmartPhones<string, decimal> { Kachestvo = kachestvo, Model = model, Price = price });
+                        }
                         break;
                     case 3:
-                        ShowAccounts(conString);
+                        {
+                            foreach (SmartPhones<string, decimal> items in smartPhones)
+                            {
+                                Console.WriteLine($"Модель: {items.Model} - Качество: {items.Kachestvo} - Цена: {items.Price}");
+                            }
+                            Console.Write("Выберите Модель Телефона :");
+
+                            var model = Console.ReadLine();
+                            /*Console.WriteLine(laptops[model].Price);*/
+                            Console.WriteLine();
+
+                            var number = 0;
+                            foreach (SmartPhones<string, decimal> items in smartPhones)
+                            {
+                                if (items.Model == model)
+                                {
+                                    Console.WriteLine($"Модель: {items.Model} - Качество: {items.Kachestvo} - Цена: {items.Price}");
+                                    Console.WriteLine($"Индекс: Элемента {++number}");
+                                }
+                                number++;
+                            }
+                        }
                         break;
                     case 4:
-                        Console.Write("From Account: ");
-                        var fromAcc = Console.ReadLine();
+                        {
+                            var number = 0;
+                            for (int i = 0; i < smartPhones.Count; i++)
+                            {
+                                if (i == smartPhones.Count - 1)
+                                {
+                                    Console.WriteLine($"Всего {++number} Телефонов");
+                                }
+                                number++;
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Something went wrong!!!");
+                        break;
+                }
+            }
 
-                        Console.Write("To Account: ");
-                        var toAcc = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Вторая программа Dictionary");
+            // 2 Задача
+            Dictionary<string, ModelsLaptop<String, decimal>> laptops = new Dictionary<string, ModelsLaptop<string, decimal>>();
 
-                        Decimal.TryParse(Console.ReadLine(), out var amount);
-                        TransferFromToAcc(fromAcc, toAcc, amount, conString);
+            laptops.Add("Lenovo", new ModelsLaptop<string, decimal> { Kachestvo = "Bad", Price = 500.00m });
+            laptops.Add("MacBook", new ModelsLaptop<string, decimal> { Kachestvo = "Nice", Price = 1000.00m });
+            laptops.Add("Dell", new ModelsLaptop<string, decimal> { Kachestvo = "Normal", Price = 800.00m });
+            laptops.Add("HP-Omen", new ModelsLaptop<string, decimal> { Kachestvo = "Normal", Price = 700.00m });
 
-                        CheckBalanceFromAcc(fromAcc, conString);
-                        CheckBalanceToAcc(toAcc, conString);
+            working = true;
+            while (working)
+            {
+                Console.WriteLine("1.Выйти\n2.Добавить Ноутбук\n3.Выберать Ноутбук по индексу\n4.Посчитать общее количество Ноутбук\nВыбор:");
+                int.TryParse(Console.ReadLine(), out var choice);
+                switch (choice)
+                {
+                    case 1:
+                        working = false;
+                        break;
+                    case 2:
+                        {
+                            Console.Write("Качество: ");
+                            var kachestvo = Console.ReadLine();
+                            Console.Write("Модель ноута: ");
+                            var model = Console.ReadLine();
+                            Console.Write("Цена: ");
+                            Decimal.TryParse(Console.ReadLine(), out var price);
+                            laptops.Add(model, new ModelsLaptop<string, decimal> { Kachestvo = kachestvo, Price = price });
+                        }
+                        break;
+                    case 3:
+                        {
+                            foreach (KeyValuePair<string, ModelsLaptop<string, decimal>> item in laptops)
+                            {
+                                Console.WriteLine($"Модель: {item.Key} - Качество: {item.Value.Kachestvo} - Цена: {item.Value.Price}");
+                            }
+                            Console.Write("Выберите Модель Ноутбука :");
+                            var model = Console.ReadLine();
+                            /*Console.WriteLine(laptops[model].Price);*/
+                            Console.WriteLine();
+
+                            var numbers = 0;
+                            foreach (KeyValuePair<string, ModelsLaptop<string, decimal>> items in laptops)
+                            {
+                                if (items.Key == model)
+                                {
+                                    Console.WriteLine($"Модель: {items.Key} - Качество: {items.Value.Kachestvo} - Цена: {items.Value.Price}");
+                                    Console.WriteLine($"Индекс: Элемента {++numbers}");
+                                }
+                                numbers++;
+                            }
+                        }
+                        break;
+                    case 4:
+                        var number = 0;
+                        for (int i = 0; i < laptops.Count; i++)
+                        {
+                            if (i == laptops.Count - 1)
+                            {
+                                Console.WriteLine($"Всего {++number} Ноутбук");
+                            }
+                            number++;
+                        }
                         break;
                     default:
                         Console.WriteLine("Something went wrong!!!");
@@ -48,310 +152,18 @@ namespace AcademyCSharpLesson21._09._2021
                 }
             }
         }
-
-        private static void CheckBalanceFromAcc(string fromAcc, string conString)
-        {
-            var sqlConnection = new SqlConnection(conString);
-            sqlConnection.Open();
-            var command = sqlConnection.CreateCommand();
-            command.CommandText = "select sum(t.Amount) from Transactions t left join Account a on t.Account_Id = a.Id where a.Account = @fromAcc";
-            command.Parameters.AddWithValue("@fromAcc", fromAcc);
-            var reader = command.ExecuteReader();
-            decimal fromAccBalance = 0m;
-
-            while (reader.Read())
-            {
-                fromAccBalance = !string.IsNullOrEmpty(reader.GetValue(0)?.ToString()) ? reader.GetDecimal(0) : 0;
-            }
-
-            reader.Close();
-            command.Parameters.Clear();
-
-            if (fromAccBalance == 0m)
-            {
-                command.Parameters.Clear();
-
-                var fromAccId = GetAccointId(fromAcc, conString);
-
-                if (fromAccId == 0)
-                {
-                    throw new Exception("Account '\'toAcc'\' not found");
-                }
-
-                command.CommandText = $"Update Account set Is_Active = 0 where Id = @fromAccId";
-                command.Parameters.AddWithValue("@fromAccId", fromAccId);
-
-                var reader2 = command.ExecuteNonQuery();
-
-                if (reader2 > 0)
-                {
-                    Console.WriteLine($"{fromAcc} balance is 0, please top up your balance");
-                }
-            }
-
-            reader.Close();
-            command.Parameters.Clear();
-
-            sqlConnection.Close();
-            //return fromAccBalance;
-        }
-        private static void CheckBalanceToAcc(string toAcc, string conString)
-        {
-            var sqlConnection = new SqlConnection(conString);
-            sqlConnection.Open();
-            var command = sqlConnection.CreateCommand();
-            command.CommandText = "select sum(t.Amount) from Transactions t left join Account a on t.Account_Id = a.Id where a.Account = @toAcc";
-            command.Parameters.AddWithValue("@toAcc", toAcc);
-            var reader = command.ExecuteReader();
-            decimal fromAccBalance = 0m;
-
-            while (reader.Read())
-            {
-                fromAccBalance = !string.IsNullOrEmpty(reader.GetValue(0)?.ToString()) ? reader.GetDecimal(0) : 0;
-            }
-
-            reader.Close();
-            command.Parameters.Clear();
-
-            if (fromAccBalance > 0m)
-            {
-                command.Parameters.Clear();
-
-                var toAccId = GetAccointId(toAcc, conString);
-
-                if (toAccId == 0)
-                {
-                    throw new Exception("Account '\'toAcc'\' not found");
-                }
-
-                command.CommandText = $"Update Account set Is_Active = 1 where Id = @toAccId";
-                command.Parameters.AddWithValue("@toAccId", toAccId);
-
-                var reader2 = command.ExecuteNonQuery();
-
-                if (reader2 > 0)
-                {
-
-                }
-            }
-
-            reader.Close();
-            command.Parameters.Clear();
-
-            sqlConnection.Close();
-            //return fromAccBalance;
-        }
-
-        private static void TransferFromToAcc(string fromAcc, string toAcc, decimal amount, string conString)
-        {
-            if (string.IsNullOrEmpty(fromAcc) || string.IsNullOrEmpty(toAcc) || amount == 0)
-            {
-                Console.WriteLine("SomeThing went wrong!!!");
-                return;
-            }
-
-            var connection = new SqlConnection(conString);
-            connection.Open();
-
-            SqlTransaction sqlTransaction = connection.BeginTransaction();
-
-            var command = connection.CreateCommand();
-
-            command.Transaction = sqlTransaction;
-
-            try
-            {
-                command.CommandText = "select sum(t.Amount) from Transactions t left join Account a on t.Account_Id = a.Id where a.Account = @fromAcc";
-                command.Parameters.AddWithValue("@fromAcc", fromAcc);
-                var reader = command.ExecuteReader();
-                var fromBalanceAcc = 0m;
-
-                while (reader.Read())
-                {
-                    fromBalanceAcc = !string.IsNullOrEmpty(reader.GetValue(0)?.ToString()) ? reader.GetDecimal(0) : 0;
-                }
-
-                reader.Close();
-                command.Parameters.Clear();
-
-                if (fromBalanceAcc <= 0 || (fromBalanceAcc - amount) < 0)
-                {
-                    throw new Exception("From account balance not enough amount");
-                }
-
-                var fromAccId = GetAccointId(fromAcc, conString);
-
-                if (fromAccId == 0)
-                {
-                    throw new Exception("Account '\'fromAcc'\' not found");
-                }
-
-                command.CommandText = "Insert into Transactions(Account_Id, Amount, Created_At) Values(@accountId, @amount, @createdAt)";
-                command.Parameters.AddWithValue("@accountId", fromAccId);
-                command.Parameters.AddWithValue("@amount", amount * -1);
-                command.Parameters.AddWithValue("@createdAt", DateTime.Now);
-
-                var result1 = command.ExecuteNonQuery();
-
-                var toAccId = GetAccointId(toAcc, conString);
-
-                if (toAccId == 0)
-                {
-                    throw new Exception("Account '\'toAcc'\' not found");
-                }
-
-                command.Parameters.Clear();
-
-                command.CommandText = "Insert into Transactions(Account_Id, Amount, Created_At) Values(@accountId, @amount, @createdAt)";
-                command.Parameters.AddWithValue("@accountId", toAccId);
-                command.Parameters.AddWithValue("@amount", amount);
-                command.Parameters.AddWithValue("@createdAt", DateTime.Now);
-
-                var result2 = command.ExecuteNonQuery();
-
-                if (result1 == 0 || result2 == 0)
-                {
-                    throw new Exception("Something went wrong!!!");
-                }
-
-                sqlTransaction.Commit();
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-
-                sqlTransaction.Rollback();
-            }
-            finally
-            {
-                connection.Close();
-            }
-        }
-
-        private static int GetAccointId(string number, string conString)
-        {
-            var accNumber = 0;
-            var sqlConnection = new SqlConnection(conString);
-            var query = "Select Id from Account where Account = @account";
-
-            var command = sqlConnection.CreateCommand();
-            command.CommandText = query;
-            command.Parameters.AddWithValue(@"account", number);
-
-            sqlConnection.Open();
-
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                accNumber = reader.GetInt32(0);
-            }
-
-            sqlConnection.Close();
-            reader.Close();
-
-            return accNumber;
-        }
-
-        private static void CreateAccount(string conString)
-        {
-            Console.Write("NickName:");
-
-            var account = new Account
-            {
-                Acount = Console.ReadLine(),
-                IsActive = 1,
-                CreatedAt = DateTime.Now,
-            };
-
-            SqlConnection sqlConnection = new SqlConnection(conString);
-            var query = "Insert Into Account(Account, Is_Active, Created_At) Values(@account, @isActive, @createdAt)";
-
-            var command = sqlConnection.CreateCommand();
-            command.CommandText = query;
-            command.Parameters.AddWithValue("@account", account.Acount);
-            command.Parameters.AddWithValue("@isActive", account.IsActive);
-            command.Parameters.AddWithValue("@createdAt", account.CreatedAt);
-
-            sqlConnection.Open();
-
-            var result1 = command.ExecuteNonQuery();
-
-            if (result1 > 0)
-            {
-                Console.WriteLine("Added Successfully");
-            }
-            else
-            {
-                Console.WriteLine("Something went wrong");
-            }
-
-            sqlConnection.Close();
-        }
-
-        private static void ShowAccounts(string conString)
-        {
-            Account[] accounts = new Account[0];
-
-            SqlConnection sqlConnection = new SqlConnection(conString);
-            var query = "Select Id, Account, Is_Active, Created_At, Updated_At from Account";
-
-            SqlCommand command = sqlConnection.CreateCommand();
-            command.CommandText = query;
-
-            sqlConnection.Open();
-
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Account account = new Account { };
-
-                account.Id = int.Parse(reader["Id"].ToString());
-                account.Acount = reader["Account"].ToString();
-                account.IsActive = int.Parse(reader["Is_Active"].ToString());
-
-                account.CreatedAt = !string.IsNullOrEmpty(reader["Created_At"]?.ToString()) ? DateTime.Parse(reader["Created_At"].ToString()) : null;
-                account.UpdatedAt = !string.IsNullOrEmpty(reader["Updated_At"]?.ToString()) ? DateTime.Parse(reader["Updated_At"].ToString()) : null;
-
-                AddClient(ref accounts, account);
-            }
-            sqlConnection.Close();
-
-            foreach (var account in accounts)
-            {
-                Console.WriteLine($"ID:{account.Id}, Account:{account.Acount}, Is_Active:{account.IsActive}, " +
-                    $"Created_At:{account.CreatedAt}, Updated_At:{account.UpdatedAt}");
-            }
-        }
-
-        private static void AddClient(ref Account[] accounts, Account account)
-        {
-            if (accounts == null)
-            {
-                return;
-            }
-
-            Array.Resize(ref accounts, accounts.Length + 1);
-
-            accounts[accounts.Length - 1] = account;
-        }
-
     }
-    class Account
+
+    public class ModelsLaptop<T, U>
     {
-        public int Id { get; set; }
-        public string Acount { get; set; }
-        public int IsActive { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+        public T Kachestvo { get; set; }
+        public U Price { get; set; }
     }
-    class Transactions
+
+    public class SmartPhones<T, Z>
     {
-        public int Id { get; set; }
-        public int AccountId { get; set; }
-        public Decimal Amount { get; set; }
-        public DateTime? CreatedAt { get; set; }
+        public T Kachestvo { get; set; }
+        public T Model { get; set; }
+        public Z Price { get; set; }
     }
 }
