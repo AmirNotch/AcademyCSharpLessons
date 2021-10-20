d10b63ff0dbbe58a6f44c419e6b53976fd12f6de
Amir Ibragimov <ibragimov-amir@list.ru>
2021-10-21 01:54:33 +0600

    12.10.2021 in one column can be double chain

---
+++ AcademyCSharpLesson12.10.2021/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AcademyCSharpLesson12._10._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixSymbols appearence;
+
+            for (int i = 0; i < 25; i++)
+            {
+                appearence = new MatrixSymbols(i * 3,true);
+                new Thread(appearence.Move).Start();
+            }
+        }
+    }
+    class MatrixSymbols
+    {
+        static object locker = new object();
+
+        Random random;
+
+        const string symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&";
+
+        public int Colunm { get; set; }
+
+        public bool NeedSecond { get; set; }
+
+        // Конструктор где прописывем колонку и ожиданиеВремени = true 
+        public MatrixSymbols(int column, bool needSecond)
+        {
+            Colunm = column;
+            random = new Random((int)DateTime.Now.Ticks);
+            NeedSecond = needSecond;
+        }
+        // Получаем рандомный символ 
+        public char GetChar()
+        {
+            return symbols.ToCharArray()[random.Next(0, symbols.Length)];
+        }
+        // Метод который запускает потоки
+        public void Move()
+        {
+            int lenght;
+            int count;
+
+            while (true)
+            {
+                count = random.Next(5, 10);
+                lenght = 0;
+                Thread.Sleep(random.Next(0, 200));
+                for (int i = 0; i < 38; i++)
+                {
+                    lock (locker)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.CursorTop = i - lenght;
+                        for (int j = i - lenght - 1; j < i; j++)
+                        {
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine("в–$");
+                        }
+
+                        if (lenght < count)
+                            lenght++;
+                        else
+                            if (lenght == count)
+                            count = 0;
+                        if (NeedSecond && i < 20 && i > lenght + 2 && (random.Next(1, 5) == 3))
+                        {
+                            new Thread((new MatrixSymbols(Colunm, false)).Move).Start();
+                            NeedSecond = false;
+                        }
+
+                        if (39 - i < lenght)
+                            lenght--;
+                        Console.CursorTop = i - lenght + 1;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        for (int j = 0; j < lenght - 2; j++)
+                        {
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());
+                            /*//Thread.Sleep(2000);
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());*/
+
+
+                            var innerTask = Task.Factory.StartNew(() => 
+                            {
+                                Thread.Sleep(8000);
+                                Console.CursorLeft = Colunm;
+                                Console.WriteLine(GetChar());
+                            });
+                        }
+                        if (lenght >= 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());
+                            /*//Thread.Sleep(2000);
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());*/
+
+                            var innerTask = Task.Factory.StartNew(() =>
+                            {
+                                Thread.Sleep(8000);
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.CursorLeft = Colunm;
+                                Console.WriteLine(GetChar());
+                            });
+                        }
+                        if (lenght >= 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());
+                            /*//Thread.Sleep(2000);
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());*/
+
+                            var innerTask = Task.Factory.StartNew(() =>
+                            {
+                                Thread.Sleep(8000);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.CursorLeft = Colunm;
+                                Console.WriteLine(GetChar());
+                            });
+                        }
+
+                        Thread.Sleep(50);
+                    }
+                }
+            }
+        }
+    }
+}
